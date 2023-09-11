d66e79b5060e640da737bb0576c21f58ea46e046
Amandito <bimooamandito@gmail.com>
2023-09-11 21:08:05 +0700

    tahun sekarang - tahun lahir

--- main.dart
+++ main.dart
@@ -6,10 +6,13 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int tahunSekarang = DateTime.now().year;
+    int usia = tahunSekarang - tahunLahir;
+    print("Perkenalkan, nama saya $nama, dengan NIM $nim, usia saya $usia tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021059", "Bimo Amandito", 2002);
+  mhs.perkenalan();
 }
